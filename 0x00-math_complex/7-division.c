1afe1ddbddc73d1c56e41114942745f9158ff457
Ella711 <allenro711@gmail.com>
2021-11-21 16:24:24 -0500

    [FIX] Parenthesis

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -11,7 +11,7 @@ void division(complex c1, complex c2, complex *c3)
 {
 	double denominator;
 
-	denominator = ((c2.re * c2.re) + (c2.im * c2.im));
+	denominator = c2.re * c2.re + c2.im * c2.im;
 
 	c3->re = ((c1.re * c2.re) + (c1.im * c2.im)) / denominator;
 	c3->im = ((c1.im * c2.re) - (c1.re * c2.im)) / denominator;
