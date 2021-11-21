90f76462a081967043623f3e89d8d4f3aa4a868e
Ella711 <allenro711@gmail.com>
2021-11-20 21:43:36 -0500

    [ADD] File for task0

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * display_complex_number - displays complex numbers
+ * @c: struct with complex numbers
+ */
+
+void display_complex_number(complex c)
+{
+	char operator;
+	float im_value;
+
+	if (c.im < 0)
+		im_value = (-(c.im));
+	else
+		im_value = c.im;
+
+	if (c.im == 0)
+		printf("%.f\n", c.re);
+
+
+	if (im_value > 0)
+	{
+		if (c.im > 0)
+			operator = '+';
+		else
+			operator = '-';
+
+		if (im_value == 1)
+			printf("%.f %c i\n", c.re, operator);
+		else
+			printf("%.f %c %.fi\n", c.re, operator, c.im);
+	}
+
+}
+
