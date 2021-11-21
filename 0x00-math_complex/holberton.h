b16516274ba9c6227323386155b3452aa43dece9
Ella711 <allenro711@gmail.com>
2021-11-20 21:42:50 -0500

    [ADD] header file

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct complex_display - struct to declare a complex number
+ * @re: real number
+ * @im: imaginary number
+ */
+
+typedef struct complex_display
+{
+	double re;
+	double im;
+} complex;
+
+/* displays the complex numbers */
+void display_complex_number(complex c);
+
+#endif /* MAIN_HEADER */
+
